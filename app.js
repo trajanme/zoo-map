d3cc0e0f01d31d233f36b216ea0ad69e8e0a84f5
trajanme <shintaro@trajan.me>
2021-03-07 21:33:50 +0900

    added filter

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const animals = [
   {
     name: 'アフリカゾウ',
-    type: '長鼻目ゾウ科アフリカゾウ属'
+    type: '長鼻目ゾウ科アフリカゾウ属',
+    english: 'African Elephant',
+    quantity: 3,
+    weight: 6000,
   },
   {
     name: 'ニホンザル',
-    type: '哺乳綱霊長目オナガザル科マカク属'
+    type: '哺乳綱霊長目オナガザル科マカク属',
+    english: 'Japanese Macaque',
+    quantity: 30,
+    weight: 11,
   }
 ]
 
@@ -13,6 +19,14 @@ var vm = new Vue ({
   el: '#app',
   data: {
     animals: animals
+  },
+  filters: {
+    numberWithDelimiter: function(value) {
+      if (!value) {
+        return '0'
+      }
+      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+    }
   }
 })
 
